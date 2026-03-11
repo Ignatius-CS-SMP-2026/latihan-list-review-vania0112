5ff5a226e37b479f1cfef2ad940b010d84661e64
vania0112 <vania.huang061211@gmail.com>
2026-03-11 12:09:36 +0700

    Selesai latihan

--- latihan_list.py
+++ latihan_list.py
@@ -16,3 +16,15 @@
 # ---------------------------------------------------------
 
 # Tulis kodemu di bawah ini:
+nilai_ujian = [75, 55, 90, 85, 45, 95, 80]
+
+print("Data nilai asli:", nilai_ujian)
+
+nilai_urut = sorted(nilai_ujian, reverse=True)
+print("Data setelah diurutkan:", nilai_urut)
+
+beasiswa = nilai_urut[:3]
+print("Tiga nilai tertinggi (Penerima Beasiswa):", beasiswa)
+
+lulus = [nilai for nilai in nilai_urut if nilai >= 60]
+print("Daftar nilai yang lulus:", lulus)
